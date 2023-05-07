49c4f89c94d8feb3b7b6cc6e793c6bb2183cc554
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2023-05-07 12:35:33 +0500

    implement api to save product

--- src/main/java/com/sibau/demo/controller/ProductController.java
+++ src/main/java/com/sibau/demo/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.sibau.demo.controller;
 import com.sibau.demo.model.Product;
 import com.sibau.demo.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +20,10 @@ public class ProductController {
     @GetMapping("/products")
     public List<Product> getProducts() {
         return productService.getProducts();
+    }
+    @PostMapping("/saveProduct")
+    public Product saveProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
     }
 
 }
