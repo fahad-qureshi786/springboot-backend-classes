9a42f61fc92cf6b16179b2a402be323495c36a73
fahad-qureshi786 <mfahad.bses19@iba-suk.edu.pk>
2023-05-07 12:25:13 +0500

    implement api

--- src/main/java/com/sibau/demo/controller/ProductController.java
+++ src/main/java/com/sibau/demo/controller/ProductController.java
@@ -1,4 +1,27 @@
 package com.sibau.demo.controller;
 
+import com.sibau.demo.model.Product;
+import com.sibau.demo.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/products")
 public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    /**
+     * http://localhost:8081/products/
+     * @return
+     */
+    @GetMapping("/")
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
 }
